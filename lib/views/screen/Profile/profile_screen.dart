9f80894ceab76869d77d1ea1122a922b9bcf8a47
mrswapon <mrswapon15@gmail.com>
2024-11-30 16:29:06 +0600

    Profile Screen Done

--- lib/views/screen/Profile/profile_screen.dart
+++ lib/views/screen/Profile/profile_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:ndolo_dating/views/base/custom_list_tile.dart';
+import 'package:ndolo_dating/views/base/custom_network_image.dart';
 import '../../../helpers/route.dart';
 import '../../../utils/app_colors.dart';
+import '../../../utils/app_icons.dart';
+import '../../../utils/app_images.dart';
+import '../../../utils/app_strings.dart';
 import '../../base/bottom_menu..dart';
 import '../../base/custom_button.dart';
 import '../../base/custom_text.dart';
@@ -12,11 +18,124 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       bottomNavigationBar: BottomMenu(3),
+      //=============================> AppBar Section <=============================
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(AppImages.appLogo, width: 121.w, height: 32.h),
+            InkWell(
+                onTap: () {},
+                child: SvgPicture.asset(AppIcons.settings,
+                    width: 32.w, height: 32.h))
+          ],
+        ),
+      ),
+      //=============================> Body Section <=============================
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: AppColors.cardColor,
+                borderRadius: BorderRadius.circular(24.r)),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.h),
+              child: Column(children: [
+                //=============================> Profile Picture <=============================
+                CustomNetworkImage(
+                  imageUrl:
+                      'https://s3-alpha-sig.figma.com/img/a1c9/575c/4f24b44129bd1c1832d68d397b792497?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GwEsZ3WRA~UZBXPSyVn1~EP56OutWf9ks3Pp5SGI6MCjGGZAFlHEi2N4IlDVFviedcItZdtfZQtgWJHPudEZFWEcFDRzdXKF-VR8B1Sbr0xTBOs2pmjcEAJUy-mGNPoh0~QXEtajPrE9MKTiQrV2581Cm0gx8yNzIXLqrRy5xdqs6nyUprsgffdi~rkm3SylakKm40tW6mCca7fwwTduZ6hzxrjf1vsbiFdkl9ntcgMN89j3zasCaMxVOa9wueLCPiablbJiC1z5lO8nY5ensKpTa5AKMLg0pS6dIDBEst9u95IIhxLBaaheZC2JrOb6aAwBzMY2hq5R1IhXkaWpfg__',
+                  height: 136.h,
+                  width: 136.w,
+                  boxShape: BoxShape.circle,
+                  border: Border.all(width: 2.w, color: AppColors.primaryColor),
+                ),
+                SizedBox(height: 16.h),
+                //=============================> Update Pictures Button <=============================
+                GestureDetector(
+                  onTap: () {
+                    Get.toNamed(AppRoutes.uploadPhotosScreen);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(4.r)),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SvgPicture.asset(AppIcons.pen),
+                          SizedBox(width: 8.w),
+                          CustomText(
+                            text: AppStrings.updatePictures.tr,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.h),
+                //=============================> Name Section <=============================
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: CustomText(
+                    text: 'Janet Doe',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30.sp,
+                    maxLine: 5,
+                    color: const Color(0xff430750),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+          SizedBox(height: 24.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Column(
+              children: [
+                //=====================> Account Information List Tile <===================
+                CustomListTile(
+                  onTap: (){},
+                  title: AppStrings.accountInformation.tr,
+                  prefixIcon: SvgPicture.asset(AppIcons.account),
+                  suffixIcon: SvgPicture.asset(AppIcons.rightArrow),
+                ),
+                //=====================> Language List Tile <===================
+                CustomListTile(
+                  onTap: (){
+                    _popupMenuButton();
+                  },
+                  title: AppStrings.language.tr,
+                  prefixIcon: SvgPicture.asset(AppIcons.lan),
+                  suffixIcon: SvgPicture.asset(AppIcons.rightArrow),
+                ),
+                //=====================> Log Out List Tile <===================
+                CustomListTile(
+                  onTap: (){
+                    _showCustomBottomSheet(context);
+                  },
+                  title: AppStrings.logOut.tr,
+                  prefixIcon: SvgPicture.asset(AppIcons.logOut),
+                  suffixIcon: SvgPicture.asset(AppIcons.rightArrow),
+                ),
+
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
-
 
   //===============================> Log Out Bottom Sheet <===============================
   _showCustomBottomSheet(BuildContext context) {
@@ -41,13 +160,21 @@ class ProfileScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
-              CustomText(text: 'Logout', fontWeight: FontWeight.w500, fontSize: 24.sp,),
+              CustomText(
+                text: AppStrings.logOut.tr,
+                fontWeight: FontWeight.bold,
+                fontSize: 24.sp,
+                color: Colors.red,
+              ),
               SizedBox(height: 20.h),
-              Divider(thickness: 1, color: AppColors.primaryColor, indent: 15.w,),
+              Divider(
+                thickness: 1,
+                color: AppColors.primaryColor,
+                indent: 15.w,
+              ),
               SizedBox(height: 20.h),
               CustomText(
-                text: 'Are you sure you want to log out?',
+                text: AppStrings.areYouSureToLogOut.tr,
                 fontSize: 16.sp,
               ),
               SizedBox(height: 20.h),
@@ -68,9 +195,11 @@ class ProfileScreen extends StatelessWidget {
                   SizedBox(width: 16.w),
                   SizedBox(
                       width: 115.w,
-                      child: CustomButton(onTap: () {
-                        Get.offAllNamed(AppRoutes.homeScreen);
-                      }, text: "Yes")),
+                      child: CustomButton(
+                          onTap: () {
+                            Get.offAllNamed(AppRoutes.homeScreen);
+                          },
+                          text: "Yes")),
                 ],
               ),
             ],
@@ -79,4 +208,53 @@ class ProfileScreen extends StatelessWidget {
       },
     );
   }
+
+
+//================================> Popup Menu Button Method <=============================
+  PopupMenuButton<int> _popupMenuButton() {
+    return PopupMenuButton<int>(
+    //  icon: const Icon(Icons.more_vert),
+      onSelected: (int result) {
+        if (result == 0) {
+          print('Report selected');
+        } else if (result == 1) {
+          print('Block selected');
+        }
+      },
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<int>>[
+        PopupMenuItem<int>(
+          value: 0,
+          child: Row(
+            children: [
+              const Icon(Icons.report, color: Colors.black54),
+              SizedBox(width: 10.w),
+              const Text(
+                'French',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+        PopupMenuItem<int>(
+          value: 1,
+          child: Row(
+            children: [
+              const Icon(Icons.block, color: Colors.black54),
+              SizedBox(width: 10.w),
+              const Text(
+                'English',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ],
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+
+
 }
