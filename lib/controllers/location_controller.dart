02a6480a741dd30d4396e164aa9524fd0bab9bfd
mrswapon <mrswapon15@gmail.com>
2025-03-17 15:26:19 +0600

    Location Issue Fix

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -14,35 +14,37 @@ class LocationController extends GetxController {
   var setLocationLoading = false.obs;
 
   setLocation({required String latitude, required String longitude}) async {
-      var body = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "locationName": locationNameController.text,
-      };
-      String bearerToken = await PrefsHelper.getString(AppConstants.bearerToken);
-      var headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $bearerToken',
-      };
-      try {
-        setLocationLoading(true);
-        Response response = await ApiClient.postData(
-            ApiConstants.setLocationEndPoint, jsonEncode(body),
-            headers: headers);
-        print("Response: ${response.body}, Status: ${response.statusCode}");
-        if (response.statusCode == 200 || response.statusCode == 201) {
-          await PrefsHelper.setBool(AppConstants.hasUpdateGallery, true);
-          Fluttertoast.showToast(msg: "Your location is set successfully");
-          Get.offAllNamed(AppRoutes.idealMatchScreen);
-        } else {
-          Fluttertoast.showToast(msg: "Failed to update location: ${response.body}");
-          print("Failed: ${response.body}");
-        }
-      } catch (e) {
-        print("Error in API call: $e");
-        Fluttertoast.showToast(msg: "Error updating location");
-      } finally {
-        setLocationLoading(false);
+    var body = {
+      "latitude": latitude,
+      "longitude": longitude,
+      "locationName": locationNameController.text,
+    };
+    String bearerToken = await PrefsHelper.getString(AppConstants.bearerToken);
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $bearerToken',
+    };
+    try {
+      setLocationLoading(true);
+      Response response = await ApiClient.postData(
+          ApiConstants.setLocationEndPoint, jsonEncode(body),
+          headers: headers);
+      print("Response: ${response.body}, Status: ${response.statusCode}");
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        await PrefsHelper.setBool(AppConstants.hasUpdateGallery, true);
+        Fluttertoast.showToast(msg: "Your location is set successfully");
+        Get.offAllNamed(AppRoutes.idealMatchScreen);  // Navigate to the next screen
+      } else {
+        Fluttertoast.showToast(msg: "Failed to update location: ${response.body}");
+        print("Failed: ${response.body}");
       }
+    } catch (e) {
+      print("Error in API call: $e");
+      Fluttertoast.showToast(msg: "Error updating location");
+    } finally {
+      setLocationLoading(false);  // Stop loading state
+    }
   }
+
 }
